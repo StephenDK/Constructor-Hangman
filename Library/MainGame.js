23cd600aa87822bc910ffeb874d99c50fff8ad9a
StephenDK <stephendklein08@gmail.com>
2018-01-30 17:02:52 -0800

    Added prompt user function

--- Library/MainGame.js
+++ Library/MainGame.js
@@ -1,15 +1,44 @@
 //  Get the inquirer npm 
 var inquirer = require("inquirer");
-
-
 // Get the words from the "wordlist.js" file
 var words = require("./wordlist");
+
+
 
 // game constructor will keep score and control the flow of the game
 function Game() {
 
     // reference point for inquirer
     var self = this;
+
+    // Prompt user for letter
+    this.askForLetter = function() {
+        return inquirer
+            .prompt([
+                {
+                    type: "input",
+                    name: "choice",
+                    message: "Guess a letter!",
+
+                }
+            ]).then(function(val) {
+                // If the users guess is correct log correct for the user
+                var guessIsCorrect = self.currentWord.guessLetter(val.choice);
+                if (guessIsCorrect) {
+                    console.log("Correct!")
+                }else {
+                    // if the guess is wrong minus guessesleft and display wron guess message
+                    self.guessesLeft--;
+                    console.log("Sorry thats incorrect!");
+                    console.log(self.guessesLeft + " guesses remaining!!")
+                }
+            });
+    }
+
+    // when game is over display "thanks for playing"
+    this.gameOver = function() {
+        console.log("Thanks for playing!!");
+    }
 
     // Set guesses to 10 and get next word
     this.play = function() {
